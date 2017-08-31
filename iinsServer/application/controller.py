2b2cb7fe2eb807dd9b6a12208aecf2c3cb460ed4
isaac <isaac.jiang@uwaterloo.ca>
2017-08-31 00:08:03 -0400

    20170830.001

---
+++ iinsServer/application/controller.py
@@ -0,0 +1,18 @@
+from . import models
+from flask import json, request
+
+
+
+class ApplicationService():
+    def __init__(self):
+        self.model = models
+
+    def get_application_list(self):
+        print(request.args)
+        results = self.model.ApplicationModel().get_application_list()
+        return json.dumps(results)
+
+    def save_application(self):
+        application = json.loads(request.data)
+        results = self.model.ApplicationModel().save_application(application)
+        return json.dumps(results)
---
+++ iinsServer/application/models.py
@@ -0,0 +1,21 @@
+from bson import ObjectId
+from pymongo import ASCENDING,MongoClient
+from flask import g
+
+class ApplicationModel():
+    def __init__(self):
+        self.collection = getattr(g,'database',MongoClient('localhost', 27017)).iins_op.application
+
+
+    def get_application_list(self):
+        result = []
+        req = self.collection.find().sort('insurer', ASCENDING)
+        if req.count() > 0:
+            for r in req:
+                r['_id'] = str(r['_id'])
+                result.append(r)
+        return result
+
+    def save_application(self,application):
+        self.collection.update_one({"policy.policyNumber":application['policy']['policyNumber']},{"$set":application},upsert=True)
+        return application
